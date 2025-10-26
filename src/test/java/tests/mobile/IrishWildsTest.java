52ec87ccae6cb8fbb426528a45db44ecd039c68a
KamboJah <sandboxqa11@gmail.com>
2025-10-26 17:18:45 +0200

    no message

--- src/test/java/tests/mobile/IrishWildsTest.java
+++ src/test/java/tests/mobile/IrishWildsTest.java
@@ -5,8 +5,10 @@ import config.Appium;
 import config.BaseTest;
 import config.ExtentTestNGListener;
 import data.Constants;
+import data.Endpoints;
 import io.appium.java_client.AppiumDriver;
 import org.testng.Assert;
+import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Listeners;
 import pages.RegistrationPage;
@@ -30,13 +32,14 @@ public class Test extends BaseTest {
         constants = new Constants();
         constants.readTestData("TestData", 1);
         Appium.startServer();
+        Appium.startEmulator();
         appiumDriver = Appium.initAppium();
     }
 
 
     @org.testng.annotations.Test(testName = "[WEB] Registration Form", description = "WEB")
     public void testRegistrationFormSubmission() throws Exception {
-        appiumDriver.get("https://demoqa.com/automation-practice-form");
+        appiumDriver.get(Endpoints.irish_wilds);
 
         ExtentTestNGListener.testPassDetails = "<pre><center><b>* * * * * * * *    User Registration Form    * * * * * * * *</b></center></br></br>"
                 + "<a href='data.xlsx'>Test Data</a></br>"
